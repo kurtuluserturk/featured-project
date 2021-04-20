8d7dbfe6b6fa5ff3c2d1ec0e891c6b81bdb63d3b
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-04-20 15:39:27 +0300

    Updated Modal and Navbar

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,38 @@ import React from 'react'
 const Modal = () => {
     return (
         <div>
-            <h2>Modal</h2>
+            <h5>Log in form</h5>
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="firstName">First name</label>
+                        <input type="text" className="form-control" id="firstName" placeholder="First name" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="lastName">Last name</label>
+                        <input type="text" className="form-control" id="lastName" placeholder="Last name" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" className="form-control" id="email" placeholder="Email" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" id="password" placeholder="Password" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck" />
+                        <label className="form-check-label" htmlFor="gridCheck">
+                            Remember me
+      </label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Log in</button>
+            </form>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo-lorem.svg'
+import Modal from './Modal'
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/">
+                <img src={logo} alt="store" className="navbar-brand" />
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <Link to="/" className="nav-link text-dark">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/contact" className="nav-link text-dark">
+                            Contact
+                        </Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Languages
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">Turkish</a>
+                            <a className="dropdown-item" href="#">English</a>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+
+                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Log in
+                        </button>
+
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                    <Modal />
+                                </div>
+                            </div>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
