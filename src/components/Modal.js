c343269e5201986b9ea48f1d6a1db219639214b6
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-04-21 22:43:02 +0300

    modal and contact updated
    modal and contact updated by using context api

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,36 +1,70 @@
 import React from 'react'
+import { useGlobalContext } from "../context";
 
 const Modal = () => {
+    const {
+        firstName,
+        lastName,
+        email,
+        user,
+        handleModalSubmit,
+        saveFirstName,
+        saveLastName,
+        saveEmail
+    } = useGlobalContext();
+
+    // if there's a user show the message below
+    if (user) {
+        return <h3>{user.firstName} is loggged in</h3>;
+    }
+
+    // if there's no user, show the login form
     return (
         <div>
-            <h5>Log in form</h5>
-            <form>
+            <h5>Log in Lorem</h5>
+            <form onSubmit={handleModalSubmit}>
                 <div className="form-row">
                     <div className="form-group col-md-6">
-                        <label htmlFor="firstName">First name</label>
-                        <input type="text" className="form-control" id="firstName" placeholder="First name" />
+                        <label htmlFor="firstName">First name:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="firstName"
+                            placeholder="First name"
+                            value={firstName}
+                            onChange={(e) => { saveFirstName(e) }}
+                            required
+                        />
                     </div>
                     <div className="form-group col-md-6">
-                        <label htmlFor="lastName">Last name</label>
-                        <input type="text" className="form-control" id="lastName" placeholder="Last name" />
+                        <label htmlFor="lastName">Last name:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="lastName"
+                            placeholder="Last name"
+                            value={lastName}
+                            onChange={(e) => { saveLastName(e) }}
+                            required
+                        />
                     </div>
                 </div>
                 <div className="form-row">
                     <div className="form-group col-md-6">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" className="form-control" id="email" placeholder="Email" />
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => { saveEmail(e) }}
+                            required
+                        />
                     </div>
                     <div className="form-group col-md-6">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="Password" />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" id="gridCheck" />
-                        <label className="form-check-label" htmlFor="gridCheck">
-                            Remember me
-                        </label>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" className="form-control" id="password" placeholder="Password" required />
                     </div>
                 </div>
                 <button type="submit" className="btn btn-primary">Log in</button>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,40 +1,92 @@
 import React from 'react'
 import Title from '../components/Title'
+import { useGlobalContext } from '../context'
 
-const contact = () => {
+const Contact = () => {
+
+    const {
+        user,
+        handleContactFormSubmit,
+        saveFirstName,
+        saveLastName,
+        saveEmail,
+        savePhone,
+        saveCountryCode,
+        saveMessage
+    } = useGlobalContext();
+
     return (
         <div className="py-5">
             <div className="container">
                 <Title name="contact" title="us" />
-                <form autoComplete="off">
+                <form onSubmit={handleContactFormSubmit}>
                     <div className="form-row d-flex justify-content-center">
                         <div className="form-group col-md-6">
                             <label htmlFor="contactFirstName">First name</label>
-                            <input type="text" className="form-control" id="contactFirstName" placeholder="First name" required />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="contactFirstName"
+                                placeholder="First name"
+                                defaultValue={user && user.firstName}
+                                onChange={(e) => { saveFirstName(e) }}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="form-row d-flex justify-content-center">
                         <div className="form-group col-md-6">
                             <label htmlFor="contactLastName">Last name</label>
-                            <input type="text" className="form-control" id="contactLastName" placeholder="Last name" required />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="contactLastName"
+                                placeholder="Last name"
+                                defaultValue={user && user.lastName}
+                                onChange={(e) => { saveLastName(e) }}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="form-row d-flex justify-content-center">
                         <div className="form-group col-md-6">
                             <label htmlFor="contactEmail">Email</label>
-                            <input type="email" className="form-control" id="contactEmail" placeholder="Email" required />
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="contactEmail"
+                                placeholder="Email"
+                                defaultValue={user && user.email}
+                                onChange={(e) => { saveEmail(e) }}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="form-row d-flex justify-content-center">
                         <div className="form-group col-md-6">
                             <label htmlFor="phone">Phone</label>
-                            <input type="tel" className="form-control" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="777-444-9999" required />
+                            <input
+                                type="tel"
+                                className="form-control"
+                                id="phone"
+                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                placeholder="777-444-9999"
+                                onChange={(e) => { savePhone(e) }}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="form-row d-flex justify-content-center">
                         <div className="form-group col-md-6">
                             <label htmlFor="myCountry">Country</label>
-                            <input type="text" className="form-control" id="myCountry" list="countryList" required />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="myCountry"
+                                list="countryList"
+                                onChange={(e) => { saveCountryCode(e) }}
+                                required
+                            />
                             <datalist id="countryList">
                                 <option value="TR">Turkey</option>
                                 <option value="US">United States of America</option>
@@ -50,7 +102,14 @@ const contact = () => {
                     <div className="form-row d-flex justify-content-center">
                         <div className="form-group col-lg-6 col-md-6">
                             <label htmlFor="contactTextarea">Your Message</label>
-                            <textarea style={{ resize: "none" }} className="form-control" id="contactTextarea" rows="3" ></textarea>
+                            <textarea
+                                style={{ resize: "none" }}
+                                className="form-control"
+                                id="contactTextarea"
+                                rows="3"
+                                onChange={(e) => { saveMessage(e) }}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="form-group d-flex justify-content-center">
@@ -62,4 +121,4 @@ const contact = () => {
     )
 }
 
-export default contact
+export default Contact
