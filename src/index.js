30883dcc5c97c94844273655e78e43598f857d08
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-04-22 14:23:01 +0300

    index.js wrapped with localization component

--- src/index.js
+++ src/index.js
@@ -5,14 +5,33 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './context'
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import common_en from "./translations/en/common.json"
+import common_tr from "./translations/tr/common.json"
+
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+  lng: 'en',                              // language to use
+  resources: {
+    en: {
+      common: common_en               // 'common' is our custom namespace
+    },
+    tr: {
+      common: common_tr
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
-      <Router>
-        <App />
-      </Router>
-    </UserProvider>
+    <I18nextProvider i18n={i18next}>
+      <UserProvider>
+        <Router>
+          <App />
+        </Router>
+      </UserProvider>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
